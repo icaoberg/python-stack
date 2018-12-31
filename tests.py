f24752c7ed3c6f6cd8cea1745aad1e90261740c7
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2018-12-31 15:18:09 -0500

    Updated code

--- tests.py
+++ tests.py
@@ -7,6 +7,13 @@ def test_empty():
 
 def test_it():
 	s = Stack()
-	for i in range(100):
+	for i in range(10):
 		s.push(i)
 		assert s.size() == i+1
+		assert s.peek() == i
+
+	for i in reversed(range(10)):
+		assert s.size() == i+1
+		assert s.peek() == i
+		assert s.pop() == i
+		assert s.size() == i
